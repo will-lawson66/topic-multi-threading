f5bb4218e6599397515cfd4d234f08820a3315e2
Bill Lawson <will.lawson66@gmail.com>
2025-04-25 09:07:40 -0400

    Add projects for exercises

---
+++ Topic-Multithreading/Code/Module 1 exercises/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int userInput = 0;
+do
+{
+    userInput = DisplayMenu();
+    switch (userInput)
+    {
+        case 1:
+            // call functional library code here;
+            break;
+        case 2:
+            // call functional...
+            break;
+        default:
+            break;
+    }
+} while (userInput != 5);
+
+
+static int DisplayMenu()
+{
+    Console.WriteLine("Sorting");
+    Console.WriteLine();
+    Console.WriteLine("1. ");
+    Console.WriteLine("2. ");
+    Console.WriteLine("3. ");
+    Console.WriteLine("4. ");
+    Console.WriteLine("5. Exit");
+    var result = Console.ReadLine();
+    return Convert.ToInt32(result);
+}
